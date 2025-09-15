45762fc0abd8d92f820784b9fe54152fce666ac2
Mojtaba Hami <mojihami87@gmail.com>
2025-09-15 10:22:54 +0330

    add fonts to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,29 @@
+import localFont from 'next/font/local';
 import "./globals.css";
+
+
+
+const yekanBakh = localFont({
+  src: [
+    {
+      path: '../../public/fonts/yekanlight.ttf', // مسیر نسبت به فایل layout.js
+      weight: '300', // Light
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/yekanmedium.ttf',
+      weight: '500', // Medium
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/yekanbold.ttf',
+      weight: '700', // Bold
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  variable: '--font-yekan-bakh', // این متغیر را برای Tailwind می‌سازیم
+});
 
 export const metadata = {
   title: "Brian Jobs",
