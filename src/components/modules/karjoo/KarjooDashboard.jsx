d2330f3c88e0351ba63f38dcc24c631f6347b236
Sizzlesina <www.sins604@gmail.com>
2025-09-19 16:15:46 +0330

    Refactor : KarjoDashboard and KarjooHeader

--- src/components/modules/karjoo/KarjooDashboard.jsx
+++ src/components/modules/karjoo/KarjooDashboard.jsx
@@ -1,8 +1,8 @@
 "use client";
 
-import React, { useState } from "react";
+import { useState } from "react";
 import MobileMenu from "@/components/layout/header/KarjooMobileMenu";
-import { LucideUser } from "lucide-react";
+import { ClipboardIcon, HomeIcon, SendIcon, UserIcon } from "lucide-react";
 
 export default function KarjooDashboard() {
   const [activeTab, setActiveTab] = useState("home");
@@ -12,78 +12,29 @@ export default function KarjooDashboard() {
     {
       id: "home",
       label: "داشبورد",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2 2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-          />
-        </svg>
-      ),
+      icon: <HomeIcon />,
     },
     {
       id: "my-resume",
       label: "رزومه من",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-          />
-        </svg>
-      ),
+      icon: <ClipboardIcon />,
       link: "/karjoo/resume",
     },
     {
       id: "sent-resumes",
       label: "رزومه‌های ارسالی",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-          />
-        </svg>
-      ),
-      link:"/karjoo/reveived-resumes",
+      icon: <SendIcon />,
+      link: "/karjoo/reveived-resumes",
     },
     {
       id: "profile",
       label: "پروفایل من",
-      icon: (
-        <LucideUser/>
-      ),
+      icon: <UserIcon />,
     },
   ];
 
-
-
   return (
-    <MobileMenu 
+    <MobileMenu
       isMobileMenuOpen={isMobileMenuOpen}
       setIsMobileMenuOpen={setIsMobileMenuOpen}
       activeTab={activeTab}
