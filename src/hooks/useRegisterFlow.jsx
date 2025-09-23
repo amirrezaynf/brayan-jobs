b77fa99108fd64c84caefc67bc9d54dbff1c128b
Sizzlesina <www.sins604@gmail.com>
2025-09-23 09:35:47 +0330

    Change to auth forms done

--- src/hooks/useRegisterFlow.jsx
+++ src/hooks/useRegisterFlow.jsx
@@ -7,7 +7,7 @@ import {
   registerStep3,
   registerStep4,
   resendVerificationCode,
-} from "@/app/action/auth";
+} from "@/app/actions/auth";
 import {
   validateContact,
   validatePassword as serverValidatePassword,
