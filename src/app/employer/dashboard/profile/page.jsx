e18561e05b85b07e4402d814916ef4592178376e
amirrezaynf <amirrezayazdanifaoo@gmail.com>
2025-09-24 14:42:11 +0330

    fix add company

--- src/app/employer/dashboard/profile/page.jsx
+++ src/app/employer/dashboard/profile/page.jsx
@@ -357,6 +357,7 @@ export default function ProfilePage() {
 
       // Try API first
       let apiResult;
+
       try {
         if (companyId) {
           apiResult = await updateCompany(companyId, dataWithToken);
